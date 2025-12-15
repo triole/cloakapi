acda4c93d2d6fa6ada35383282063fa16a9493ef
olaf michaelis <o.mic@web.de>
2025-12-15 22:01:15 +0100

    Improve logging

--- src/remove.go
+++ src/remove.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/triole/logseal"
 )
 
@@ -10,7 +8,14 @@ func (kc tKC) removeUser(userID string) {
 	user, err := kc.getUserByID(userID)
 	kc.Lg.IfErrWarn("won't remove user", logseal.F{"error": err})
 	if err == nil && user != nil {
-		fmt.Printf("remove user: %s, %s, %s\n", *user.Username, *user.Email, *user.ID)
+		kc.Lg.Info(
+			"remove user",
+			logseal.F{
+				"username": deref(user.Username),
+				"email":    deref(user.Email),
+				"id":       deref(user.ID),
+			},
+		)
 		kc.Session.Client.DeleteUser(
 			kc.Session.CTX, kc.Session.Token.AccessToken, kc.Conf.Realm, *user.ID,
 		)
